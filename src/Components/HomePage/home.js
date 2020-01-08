f5d8607a64b0c91e5bafe4f2807b5c682e78affe
Mr.Boris <borisprogrammerjs@gmail.com>
2020-01-08 18:01:02 +0200

    navigation-task

---
+++ src/Components/HomePage/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './home.css';
+
+export default class Home extends React.Component {
+   render() {
+      return (
+         <div className='container' >
+            <div className='formUser'>
+            </div>
+         </div>
+      );
+   }
+}
---
+++ src/Components/Toolbar/toolbar.js
@@ -0,0 +1,33 @@
+ import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import Home from '../HomePage/home';
+import Resume from '../Resume/resume';
+
+import './toolbar.css';
+
+export default function Toolbar() {
+   return (
+      <div className='container'>
+         <header className='toolbar'>
+            <Router>
+               <div className='navigation'>
+                  <ul>
+                     <li><Link to='/home'>Home</Link></li>
+                     <li><Link to='/resume'>Resume</Link></li>
+                  </ul>
+                  <div className='component'>
+                     <Route exact path='/home' component={Home} />
+                     <Route path='/resume' component={Resume} />
+                  </div>
+               </div>
+            </Router>
+         </header>
+      </div>
+   );
+}
+
